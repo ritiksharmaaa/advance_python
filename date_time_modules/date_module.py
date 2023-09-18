1927097b1d84b113fdece1562f7a046265d28f7f
Ritik sharma <ritik1832004@gmail.com>
2023-09-18 12:31:59 +0530

    work with some date module and sleep methods

---
+++ date_time_modules/date_module.py
@@ -0,0 +1,46 @@
+# date object contain informatoin of year month day 
+
+# @ 0bject conatin , date , main , day it a compulsory 
+
+# normal val which which use month 12 day 31 year 365, 
+
+# d = date(year , month , day  
+
+# methods
+
+# todya () , 
+
+from datetime import date  , time 
+
+dt =date(2034 , 7 , 2 )
+print(dt)
+
+
+
+# objects of dirrfw=ent class time object 
+
+# obj = contain , hour , minute , second microsecond , txinfo , * 
+
+# mean yiu give your time and yiu get own name just ex you give time and you can get how many dat the time have .
+
+
+
+# ------------time deleta class ---------------------
+
+# tell diffrence and duration between time  and dat . it is possible to know the futut=re data or prrevious date using timedelta . 
+
+# object naem = containe - day , seconds , microseconds , minutes . ( argument 0 float and may be ostive or negative number . )
+
+# it normal use to convert time in hour minutes miccroseconds ,  check val whay convert what values . 
+
+
+
+# the working with time is borning so skip or read articles . 
+
+
+
+# impo  sleap methods ----- 
+
+
+
+
