85adca22b6468e24ae6f6384ab2f69d7777e5778
Ritik sharma <ritik1832004@gmail.com>
2023-09-18 15:04:45 +0530

    learning multitasking | threading | multithreading | mainthread | creatingthreadig | get and set threadName| without class making thread

---
+++ multitasking/creating-thread/.py
@@ -0,0 +1,54 @@
+# creating thread without class 
+from threading import Thread , current_thread 
+def disp(a , b):
+    print("iam  child thread threading " , a , b)
+
+t = Thread(target=disp , args=(10 , 20 )) # here tubple single when you put ,  or / this is excute via main thread 
+# t.start() # or hera the both main and new are seprate . 
+# print(t.start())
+
+# for i in range(10):
+#     t=Thread(target=disp , args=(12 , 10))
+#     t.start()
+
+
+
+# here child thread wotk inside the main thread , but only start one they start our child threaad , they both are alag ho jayege . 
+
+
+def d():
+    for i in range(5):
+        print(" child thread ")
+b = Thread(target=d)
+b.start() # start thread 
+for i in range(5):
+    print("mainthread")
+
+    # otput came chil dhreat main thread un pattern because they both are running parlal . 
+
+
+# ------------------------------- threading with getnad set and property name ------------ 
+
+# please check latest doc to access it name . 
+
+
+def fun():
+    print(" child thread object ---------------------" , current_thread().name)
+    print("++++++++++++++++++++")
+    current_thread().name = "flying"
+    print(" child thread object ---------------------" , current_thread().name)
+
+
+n = Thread(target= fun )
+n.start()
+
+
+print("main thread------------------------------ " , current_thread().name)
+
+# here output are not get in proper way because here programe run simitniously so ine is just start suddenly second give ther own resulr =t .
+
+
+
+
+
+    
---
+++ multitasking/mainthread.py
@@ -0,0 +1,7 @@
+import threading 
+
+t = threading.current_thread().getName()
+
+print("ritit sharma")
+print(t) # it give you name mainthread . 
+
