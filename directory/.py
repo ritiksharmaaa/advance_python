c49663d604aa7bf8cc16caba62b80de058e33226
Ritik sharma <ritik1832004@gmail.com>
2023-10-01 11:57:49 +0530

    learning osmodule and how to work with dirs

---
+++ directory/.py
@@ -0,0 +1,8 @@
+import os 
+
+# print ("-------------" , os.getcwd() , " i am dir ")
+# os.mkdir("mydir") als / more /moredir / it created 
+# os.mkdirs() this is used to make a hiraracy folder .
+
+#all method is txt use it 
+
